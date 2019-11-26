029df99a55e275b180b112e8549c39a070587718
Student <student@v314-ent-017.local>
2019-11-26 11:04:40 -0500

    Final Project

---
+++ Assets/Bandits - Pixel Art/Demo/ShooterScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterScript : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        //shooting logic
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+
+    /*private void Flip()
+    {
+        m_FacingRight = !m_FacingRight;
+        transform.Rotate(0f, 180f, 0f);
+
+    }*/
+}
---
+++ Assets/Bandits - Pixel Art/Demo/bullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public int health = 100;
+    public GameObject deathEffect;
+    public int damage = 40;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+       /* Debug.Log(hitInfo.name);
+        //Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage();
+        }
+        Destroy(gameObject);
+        */
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+             if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
